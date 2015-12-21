2bbd0fad729350e6cba7ababe1cbbda3f5a9ce93
huming <humings@gmail.com>
2015-12-21 22:08:59 +0800

    fix company view model select

--- app.js
+++ app.js
@@ -5,67 +5,6 @@ function initMap() {
   });
 }
 
-// function initMapMarkers(locations) {
-//   window.mapMarkers = locations.map(function(location) {
-
-//     var contentString = '<div id="content">'+
-//     '<div id="siteNotice">'+
-//     '</div>'+
-//     '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-//     '<div id="bodyContent">'+
-//     '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-//     'sandstone rock formation in the southern part of the '+
-//     'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-//     'south west of the nearest large town, Alice Springs; 450&#160;km '+
-//     '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-//     'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-//     'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-//     'Aboriginal people of the area. It has many springs, waterholes, '+
-//     'rock caves and ancient paintings. Uluru is listed as a World '+
-//     'Heritage Site.</p>'+
-//     '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-//     'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-//     '(last visited June 22, 2009).</p>'+
-//     '</div>'+
-//     '</div>';
-
-//     var marker = new google.maps.Marker({
-//       map: map,
-//       position: {lat: location.lat, lng: location.lng},
-//       title: location.name
-//     });    
-
-//     var infowindow = new google.maps.InfoWindow({
-//       content: contentString
-//     });
-
-//     marker.addListener('click', function() {
-//       infowindow.open(map, marker);
-//     });
-//     return marker; 
-//   })
-// }
-
-// function markerIsInLocations(marker, locations) {
-//   var title = marker.getTitle();
-//   var match = false;
-//   locations.forEach(function(loc) {
-//     if (loc.name === title) {
-//       match = true;
-//     }
-//   });
-//   return match;
-// }
-
-// function updateMapMarkers(locations) {
-//   mapMarkers.forEach(function(marker) {
-//     if (markerIsInLocations(marker, locations) === false)
-//       marker.setMap(null);
-//     else
-//       marker.setMap(window.map);
-//   });
-// }
-
 function CompanyViewModel(company) {
   var self = this;
 
@@ -73,24 +12,55 @@ function CompanyViewModel(company) {
   self.lat = company.lat;
   self.lng = company.lng;
 
-  self.mapMarker = new google.maps.Marker({
-    map: map,
-    position: {lat: company.lat, lng: company.lng},
-    title: company.name
-  });
-
+  // isSelect observable
   self.isSelected = ko.observable(false);
-
   self.select = function() {
     self.isSelected(true);
-    console.log(self, self.isSelected());
+    self.mapMarker.setMap(window.map);
+    self.infoWindow.open(map, self.mapMarker);
   }
 
   self.unselect = function() {
     self.isSelected(false);
-    console.log(self, self.isSelected());
-
+    self.mapMarker.setMap(null);
   }
+
+  // mapMarker
+  self.mapMarker = new google.maps.Marker({
+    map: window.map,
+    position: {lat: company.lat, lng: company.lng},
+    title: company.name
+  });
+
+  var contentString = '<div id="content">'+
+  '<div id="siteNotice">'+
+  '</div>'+
+  '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+  '<div id="bodyContent">'+
+  '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+  'sandstone rock formation in the southern part of the '+
+  'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+  'south west of the nearest large town, Alice Springs; 450&#160;km '+
+  '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+  'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+  'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+  'Aboriginal people of the area. It has many springs, waterholes, '+
+  'rock caves and ancient paintings. Uluru is listed as a World '+
+  'Heritage Site.</p>'+
+  '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+  'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+  '(last visited June 22, 2009).</p>'+
+  '</div>'+
+  '</div>';
+
+  self.infoWindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  self.mapMarker.addListener('click', function() {
+    self.infoWindow.open(map, self.mapMarker);
+  });
+
 }
 
 function AppViewModel(companies) {
@@ -100,7 +70,8 @@ function AppViewModel(companies) {
 
   self.companies = ko.observableArray();
   companies.forEach(function(company) {
-    self.companies.push(new CompanyViewModel(company));
+    vm = new CompanyViewModel(company)
+    self.companies.push(vm);
   });
 
   self.filteredCompanies = ko.computed(function() {
@@ -123,14 +94,11 @@ function AppViewModel(companies) {
   })
 
   self.select = function(company) {
-    // console.log(company)
-    self.companies().forEach(function(c) {
+    self.companies().forEach(function(c) {      
       if (c === company) {
-        console.log('select');
-        company.select();
+        c.select();
       } else {
-        console.log('unselect');
-        company.unselect();
+        c.unselect();
       }      
     });
   }
