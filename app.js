c3562f35b51853625c6fb02cfa5c56822436ad25
huming <humings@gmail.com>
2015-12-25 21:47:53 +0800

    fix issues based on comments

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ function initMap() {
     center: {lat: 37.3876, lng: -122.0575},
     zoom: 9
   });
+
+  $(function(){
+
+    var companies = [
+      {name: 'Udacity', lat: 37.40284, lng: -122.108335},
+      {name: 'Google', lat: 37.427223, lng: -122.070993},
+      {name: 'Twitter', lat: 37.382153, lng: -122.034223},
+      {name: 'Facebook', lat: 37.485094, lng: -122.146364},
+      {name: 'Intel', lat: 37.396046, lng: -121.883039}
+    ];
+
+    vm = new AppViewModel(companies);
+
+    ko.applyBindings(vm);
+
+    window.vm = vm;
+  });
 }
 
 function CompanyViewModel(company) {
@@ -22,7 +39,9 @@ function CompanyViewModel(company) {
 
   self.unselect = function() {
     self.isSelected(false);
-    self.mapMarker.setMap(null);
+    // self.mapMarker.setMap(null);
+    if (self.infoWindow)
+      self.infoWindow.close();
   }
 
   // mapMarker
@@ -32,48 +51,63 @@ function CompanyViewModel(company) {
     title: company.name
   });
 
-  $.ajax( {
-    url: "https://en.wikipedia.org/w/api.php",
-    data: {
-      action: "query", 
-      titles: self.name, 
-      prop: "revisions|categories", 
-      rvprop: "content",
-      list: "categorymembers",
-      cmtitle: "Category:Company",
-      format: "json"
-    },
-    dataType: 'jsonp',
-    type: 'GET',
-    headers: { 'Api-User-Agent': 'Example/1.0' },
-    success: function(data) {
-       $.ajax({
+
+
+  var queryData = $.ajax({
         url: "https://en.wikipedia.org/w/api.php",
         data: {
-          action: "parse",
-          pageid: Object.keys(data.query.pages)[0],
-          contentmodel: "wikitext",
-          prop: "text",
-          disableeditsection: true,
-          noimages: true,
-          mobileformat: true,
+          action: "query", 
+          titles: self.name, 
+          prop: "revisions|categories", 
+          rvprop: "content",
+          list: "categorymembers",
+          cmtitle: "Category:Company",
           format: "json"
         },
         dataType: 'jsonp',
-        success: function(data) {
-          self.infoWindow = new google.maps.InfoWindow({
-            content: data.parse.text["*"]
-          });
+        type: 'GET',
+        headers: { 'Api-User-Agent': 'Example/1.0' }    
+      });
+  
 
-          self.mapMarker.addListener('click', function() {
-            self.infoWindow.open(map, self.mapMarker);
-          });
+  var parseData = function(data) {
+    console.log("going to parse data...")
+    $.ajax({
+      url: "https://en.wikipedia.org/w/api.php",
+      data: {
+        action: "parse",
+        pageid: Object.keys(data.query.pages)[0],
+        contentmodel: "wikitext",
+        prop: "text",
+        disableeditsection: true,
+        noimages: true,
+        mobileformat: true,
+        format: "json"
+      },
+      dataType: 'jsonp'
+    }).
+    done(function(data) {
+      console.log("parsed data is back..")
 
-        }
-       })
-    }
-} );
+      self.infoWindow = new google.maps.InfoWindow({
+        content: data.parse.text["*"]});
 
+      self.mapMarker.addListener('click', function() {
+        self.infoWindow.open(map, self.mapMarker);
+      });
+
+      self.infoWindow.addListener('closeclick', function() {
+        self.unselect();
+      });
+
+    });    
+  }
+
+  queryData
+  .then(parseData)
+  .fail(function() {
+    alert("error")
+  })
 }
 
 function AppViewModel(companies) {
@@ -89,6 +123,8 @@ function AppViewModel(companies) {
 
   self.filteredCompanies = ko.computed(function() {
     var currentCompanies = self.companies().filter(function(company) {
+      // Select companies have the same name as the search keyword, the 
+      // match should be case insentive.
       if (company.name.toLowerCase().indexOf(self.keyWord().toLowerCase()) > -1)
         return true
       else
@@ -118,17 +154,3 @@ function AppViewModel(companies) {
 
 };
 
-$(function(){
-
-  var companies = [
-    {name: 'Udacity', lat: 37.40284, lng: -122.108335},
-    {name: 'Google', lat: 37.427223, lng: -122.070993},
-    {name: 'Twitter', lat: 37.382153, lng: -122.034223},
-    {name: 'Facebook', lat: 37.485094, lng: -122.146364},
-  ];
-
-  vm = new AppViewModel(companies);
-
-  ko.applyBindings(vm);
-
-})
